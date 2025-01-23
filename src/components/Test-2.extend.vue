d07bbf1c8d9a68e8f0baafd4de7b360ba9d37dd7
Eranosyan Eduard <EranosyanEduard@yandex.ru>
2025-01-23 17:14:12 +0300

    foo

---
+++ src/components/Test-2.extend.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>foo</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import TestDefine from "./Test-1.define.vue";
+
+export default Vue.extend({
+  name: "TestExtend1",
+  mixins: [TestDefine],
+  props: {
+    testExtendProps: Boolean,
+  },
+  data() {
+    return {
+      testExtend: true,
+    };
+  },
+  computed: {
+    cTestExtend(): boolean {
+      return this.testExtend;
+    },
+  },
+  methods: {
+    showTestExtend(): void {
+      console.log(this.testExtend);
+    },
+    // HERE
+    testSuccess() {
+      console.log(this.testDefineProps);
+      console.log(this.testDefine);
+      console.log(this.cTestDefine);
+      this.showTestDefine();
+    },
+  },
+});
+</script>
