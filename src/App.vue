ab4f873399907628e30fb614a33d3ed39c5bb3d1
Eranosyan Eduard <EranosyanEduard@yandex.ru>
2025-01-29 11:33:14 +0300

    foo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/async-comps/TheAsyncCompsParent.vue'
+import { useCounterStore } from "./stores/counter";
 </script>
 
 <template>
   <div id="app">
-    <HelloWorld msg="You did it!" />
+    <!-- <HelloWorld msg="You did it!" /> -->
+    counter:
+    <button @click="useCounterStore().increment()">
+      {{ useCounterStore().counter }}
+    </button>
+    <br />
+    counter obj:
+    <button
+      @click="useCounterStore().assign({ counter: useCounterStore().counter })"
+    >
+      {{ useCounterStore().obj }}
+    </button>
+    <br />
+    counter arr:
+    <button
+      @click="useCounterStore().assignArr([{ counter: 1 }])"
+    >
+      {{ useCounterStore().arr }}
+    </button>
   </div>
 </template>
-
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,16 +1,52 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
+
+const task = "mykey";
+const getItemFromStorage = (key: string): string => {
+  console.log('key', key)
+  const state = localStorage.getItem(task);
+  return state ? JSON.stringify(JSON.parse(state)?.[key]) ?? "" : "";
+};
+
+const setItemToStorage = (key: string, value: string): void => {
+  console.log('key', key, 'value', value)
+  const data = localStorage.getItem(task);
+  const state: any = data ? JSON.parse(data ?? "") : {};
+  state[key] = JSON.parse(value);
+  localStorage.setItem(task, JSON.stringify(state));
+};
 
 export const useCounterStore = defineStore({
-  id: 'counter',
+  id: "counter",
   state: () => ({
-    counter: 0
+    counter: 0,
+    obj: {},
+    arr: [],
   }),
-  getters: {
-    doubleCount: (state) => state.counter * 2
-  },
   actions: {
     increment() {
-      this.counter++
-    }
-  }
-})
+      this.counter++;
+    },
+    assign(o: any) {
+      this.obj = { ...this.obj, ...o };
+    },
+    assignArr(o: any) {
+      this.arr = [...this.arr, ...o];
+    },
+  },
+  persist: {
+    storage: {
+      getItem(key: string): string | null {
+        return getItemFromStorage(key);
+      },
+      setItem(key: string, value: string): void {
+        return setItemToStorage(key, value);
+      },
+    },
+    beforeHydrate(context) {
+      console.log("beforeHydrate", context);
+    },
+    afterHydrate(context) {
+      console.log("afterHydrate", context);
+    },
+  },
+});
